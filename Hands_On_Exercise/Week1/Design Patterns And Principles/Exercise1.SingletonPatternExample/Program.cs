027b8b42c38d62473c45c1532092b130c27f6eb3
Amrita Kumari <amritakumari170105@gmail.com>
2025-06-20 11:51:08 +0530

    Add files via upload

---
+++ Hands_On_Exercise/Week1/Design Patterns And Principles/Exercise1.SingletonPatternExample/Program.cs
@@ -0,0 +1,25 @@
+﻿// Program.cs
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        logger1.Log("First message");
+
+        Logger logger2 = Logger.GetInstance();
+        logger2.Log("Second message");
+
+        // Check if both logger instances are the same
+        if (object.ReferenceEquals(logger1, logger2))
+        {
+            Console.WriteLine("Both logger instances are the same. Singleton works!");
+        }
+        else
+        {
+            Console.WriteLine("Different logger instances. Singleton failed!");
+        }
+    }
+}
+
