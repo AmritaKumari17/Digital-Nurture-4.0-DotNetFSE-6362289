b27cc5840dce23a7a6050832a488af48102513df
Amrita Kumari <amritakumari170105@gmail.com>
2025-07-04 11:13:28 +0530

    Add files via upload

---
+++ Hands_On_Exercise/Week3/Entity Framework Core 8.0/Lab2/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=YOUR_SERVER_NAME;Database=RetailDb;" +
+            "Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+}
