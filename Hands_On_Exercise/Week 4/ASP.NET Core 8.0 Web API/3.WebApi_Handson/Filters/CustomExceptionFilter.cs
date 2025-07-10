0b7ebf0f04d802e25e78c5bf327eb6a8f01521c0
Amrita Kumari <amritakumari170105@gmail.com>
2025-07-10 15:12:57 +0530

    Add files via upload

---
+++ Hands_On_Exercise/Week 4/ASP.NET Core 8.0 Web API/3.WebApi_Handson/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.IO;
+
+namespace EmployeeApiDemo.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var error = context.Exception;
+            File.AppendAllText("error_log.txt", $"[{DateTime.Now}] {error.Message}\n");
+
+            context.Result = new ObjectResult("An unexpected error occurred.")
+            {
+                StatusCode = 500
+            };
+        }
+    }
+}
